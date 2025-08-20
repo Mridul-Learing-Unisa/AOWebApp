bf8194ba89c020b727222e185c2fb25b069899ba
Mridul Chopra <chomy077@mymail.unisa.edu.au>
2025-08-21 05:43:11 +0930

    Week 3 - Prac

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AOWebApp.Models;
 
 public partial class Customer
 {
     public int CustomerId { get; set; }
+
+    public string FullName => FirstName + " " + LastName;
+    [NotMapped]
+    
 
     public string FirstName { get; set; } = null!;
 
@@ -24,4 +29,9 @@ public partial class Customer
     public virtual ICollection<CustomerOrder> CustomerOrders { get; set; } = new List<CustomerOrder>();
 
     public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+    public static implicit operator Customer(List<Customer> v)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ ViewModels/CustomeSearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AOWebApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AOWebApp.ViewModels
+{
+	public class CustomeSearchViewModel
+	{
+		public object searchName { get; set; }
+		public object searchSuburb { get; set; }
+        public SelectList suburbs { get; set; }
+        public List<Customer> customerList { get; set; }
+        public CustomeSearchViewModel()
+		{
+		}
+	}
+}
+
